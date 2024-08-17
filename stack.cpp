03a695523293fb72efa91eeb183d7ec6f18169f7
50915136 <wasd26314@gmail.com>
2024-08-17 18:10:46 +0800

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -22,9 +22,11 @@ private:
 
 // 建構函數
 template <class T>
+//初始化堆疊的容量為 stackCapacity，並將 top 初始化為 -1，表示堆疊為空。
 Stack<T>::Stack(int stackCapacity) : capacity(stackCapacity), top(-1)
 {
-    if (capacity <= 0) {
+    if (capacity <= 0) 
+    {
         throw invalid_argument("堆疊容量必須大於0");
     }
     stack = new T[capacity]; // 動態分配堆疊數組
@@ -51,9 +53,11 @@ T& Stack<T>::Top() const
 template <class T>
 void Stack<T>::Push(const T& item)
 {
-    if (top == capacity - 1) { // 如果堆疊已滿，擴充容量
+    if (top == capacity - 1) 
+    { // 如果堆疊已滿，擴充容量
         T* newStack = new T[2 * capacity]; // 新容量為原容量的兩倍
-        for (int i = 0; i <= top; ++i) {
+        for (int i = 0; i <= top; ++i) 
+        {
             newStack[i] = stack[i]; // 複製原有元素
         }
         delete[] stack; // 釋放原有數組
@@ -83,8 +87,9 @@ int Stack<T>::Size() const
 // 示範主函數
 int main()
 {
+    //// 創建一個整數型別的堆疊，推入 1、2、3 三個元素。
     try {
-        Stack<int> s; // 創建一個整數型別的堆疊
+        Stack<int> s; 
         s.Push(1);
         s.Push(2);
         s.Push(3);
