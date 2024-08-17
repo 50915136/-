90cf0ef2ff1d8eab14fd11402a83ebdb687db132
50915136 <wasd26314@gmail.com>
2024-08-17 21:48:02 +0800

    Update 小考2第三題.cpp

--- 小考2第三題.cpp
+++ 小考2第三題.cpp
@@ -1,80 +1,26 @@
 template <class T>
-class Chain 
+class Chain;
+
+template <class T>
+//ChainNode 類別是模板類別，
+//允許 ChainNode 節點存儲任意類型 T 的資料。
+class ChainNode 
 {
-   public:
-    Chain() { first = 0; }  // constructor, empty chain
-    ~Chain();               // destructor
-    bool IsEmpty() const { return first == 0; }
-    // other methods defined here
-    int IndexOf(const T& theElement) const;
-    void Delete(int theIndex);
-    void Insert(int theIndex, const T& theElement);
+//Chain 類別可以訪問 ChainNode 類別的私有成員
+    friend class Chain<T> public :
+        
+        chainNode() {}//創建 ChainNode 節點但不提供任何參數時使用
+//接受一個參數 data，並將它賦值給 this->data
+    ChainNode(const T& data) { this - < data = data; }
+//接受兩個參數：一個是 data，另一個是指向下一個鏈結點的指針 link。
+    ChainNode(const T& data, ChainNode<T>* link) 
+    {
+       //分別賦值給 this->data 和 this->link。
+        this->data = data;
+        this->link = link;
+    }
 
    private:
-    ChainNode<T>* first;
+    T data;//節點中的資料
+    ChainNode<T>* link;//指向下一個鏈結點的指針
 };
-
-template <class T>
-int Chain<T>::IndexOf(const T& theElement) const 
-{
-    // search the chain for theElement
-    ChainNode<T>* currentNode = first;
-    int index = 0;  // index of currentNode
-    while (currentNode != NULL && currentNode->data != theElement) 
-    {
-        // move to next node
-        currentNode = currentNode->link;
-        index++;
-    }
-    // make sure we found matching element
-    if (current == NULL)
-        return -1;
-    else
-        return index;
-}
-
-template <class T>
-void Chain<T>::Delete(int theIndex) 
-{
-    if (first == 0) throw "Cannot delete from empty chain";
-    ChainNode<T>* deleteNode;
-    if (theIndex == 0) 
-    {  // remove first node from chain
-        deleteNode = first;
-        first = first->link;
-    } 
-    else
-    {  // use p to get beforeNode
-        ChainNode<T>* p = first;
-        for (int i = 0; i < theIndex - 1; i++) 
-        {
-            if (p == 0) throw "Delete element does not exist";
-            p = p->link;
-        }
-        deleteNode = p->link;
-        p->link = p->link->link;
-    }
-    delete deleteNode;
-}
-
-template <class T>
-void Chain<T>::Insert(int theIndex, const T& theElement) 
-{
-    if (theIndex < 0) throw "Bad insert index";
-    if (theIndex == 0) 
-    {
-        // insert at front
-        first = new ChainNode<T>(theElement, first);
-    } 
-    else 
-    {  // find predecessor of new element
-        ChainNode<T>* p = first;
-        for (int i = 0; i < theIndex; i++) 
-        {
-            if (p == 0) throw "Bad insert index";
-            p = p->link;
-        }
-        // insert after p
-        p->link = new ChainNode<T>(theElement, p->link);
-    }
-}
